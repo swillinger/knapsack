680a0f04e8daf1b01b4100cd3c4467fa451ba3bc
Evan Lovely <evanlovely@gmail.com>
2018-10-06 13:38:56 -0700

    refactor(crux): removing all crux pieces

--- crux-assets/_patterns/03-components/social/social-schema.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  $schema: 'http://json-schema.org/draft-07/schema',
-  type: 'object',
-  description: 'Collection of Social Media Icons',
-  title: 'Social Media Icons',
-  properties: {},
-  examples: [{}],
-};
--- crux-assets/js/crux.js
+++
@@ -1,7 +0,0 @@
-import site from '../_patterns/02-layouts/site/site';
-import utilities from '../_patterns/00-styleguide/utilities/utilities';
-
-document.addEventListener('DOMContentLoaded', () => {
-  site();
-  utilities();
-});

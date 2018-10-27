135f8f8383cf0c94e7b8eb97a09f32c172a83332
Evan Lovely <evanlovely@gmail.com>
2018-10-27 16:40:31 -0700

    feat(create-bedrock): can take cli args

--- packages/create-bedrock/lib/init.js
+++ packages/create-bedrock/lib/init.js
@@ -1,39 +1,46 @@
-const inquirer = require('inquirer');
-const del = require('del');
+// const inquirer = require('inquirer');
+// const del = require('del');
+const { existsSync, mkdir } = require('fs-extra');
 const twigGen = require('../generators/twig');
 
-const GENERATORS = {
-  twig: 'twig',
-};
+// const GENERATORS = {
+//   twig: 'twig',
+// };
 
 async function init(options) {
-  console.log('init start');
-  if (options.force) {
-    await del(['**', '.*']);
+  if (existsSync(options.dir)) {
+    console.error('Uh oh, that directory already exists: ', options.dir);
+    process.exit(1);
   }
-  const answers = await inquirer.prompt([
-    {
-      type: 'input',
-      name: 'name',
-      default: 'my-awesome-design-system',
-      filter: input => input.toLowerCase().replace(/ /g, '-'),
-    },
-    {
-      type: 'list',
-      name: 'generator',
-      choices: Object.keys(GENERATORS),
-    },
-  ]);
+  await mkdir(options.dir);
+  // console.log('init start');
+  // if (options.force) {
+  //   await del(['**', '.*']);
+  // }
+  // const answers = await inquirer.prompt([
+  //   {
+  //     type: 'input',
+  //     name: 'name',
+  //     default: 'my-awesome-design-system',
+  //     filter: input => input.toLowerCase().replace(/ /g, '-'),
+  //   },
+  //   {
+  //     type: 'list',
+  //     name: 'generator',
+  //     choices: Object.keys(GENERATORS),
+  //   },
+  // ]);
 
-  console.log({ answers });
+  // console.log({ answers });
 
-  switch (answers.generator) {
-    case GENERATORS.twig:
-      return twigGen(answers);
-    default:
-      console.log('uh oh, no good generator selected...');
-      process.exit(1);
-  }
+  // switch (answers.generator) {
+  //   case GENERATORS.twig:
+  //     return twigGen(answers);
+  //   default:
+  //     console.log('uh oh, no good generator selected...');
+  //     process.exit(1);
+  // }
+  return twigGen(options);
 }
 
 module.exports = init;

136ad470a7b0c7fcf0ca3070e0b5622a2c01f1a0
Evan Lovely <evanlovely@gmail.com>
2018-10-27 15:39:34 -0700

    build(storybook): fix webpack build

--- apps/storybook/stories/dos-and-donts.story.jsx
+++ apps/storybook/stories/dos-and-donts.story.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import DosAndDonts from '@basalt/bedrock-dos-and-donts';
+import DosAndDonts from '../../../bedrock-site/src/components/dos-and-donts';
 
 storiesOf('DosAndDonts', module).add(
   'Overview',

d9bda92e1d14125eb2c60c28d887eb9145ada40f
Evan Lovely <evanlovely@gmail.com>
2018-11-07 21:42:37 -0800

    refactor(template): allow Twig Template to use any template string for now

--- components/twig/src/index.jsx
+++ components/twig/src/index.jsx
@@ -4,15 +4,15 @@ import Twig from './twig';
 // @todo rename package from `@basalt/bedrock-twig` to `@basalt/bedrock-template`
 
 const Template = props => {
-  let TheTemplate;
+  const TheTemplate = Twig;
   // @todo add React support
-  if (props.template.endsWith('.twig')) {
-    TheTemplate = Twig;
-  } else {
-    console.error(
-      'uh oh!!! Template did not return Twig. Fix that and then get a better error :P @todo',
-    ); // @todo
-  }
+  // if (props.template.endsWith('.twig')) {
+  //   TheTemplate = Twig;
+  // } else {
+  //   console.error(
+  //     'uh oh!!! Template did not return Twig. Fix that and then get a better error :P @todo',
+  //   ); // @todo
+  // }
   return <TheTemplate {...props} />;
 };
 

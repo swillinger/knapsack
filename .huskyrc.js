68a674142f34f112eeed70f7bde7372eea0b2994
Evan Lovely <evanlovely@gmail.com>
2018-11-08 13:49:01 -0800

    disable husky for now

---
+++ .huskyrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  hooks: {
+    // @todo re-enable
+    // 'pre-commit': 'lint-staged',
+    // 'pre-push': 'yarn test',
+    // 'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
+  },
+};

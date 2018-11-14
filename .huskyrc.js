9ca80663cd769bf199e3088ecf9365fa78e09037
Evan Lovely <evanlovely@gmail.com>
2018-11-14 14:36:43 -0800

    chore(lint): re-enable git hook tests

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,8 +1,7 @@
 module.exports = {
   hooks: {
-    // @todo re-enable
-    // 'pre-commit': 'lint-staged',
-    // 'pre-push': 'yarn test',
+    'pre-commit': 'lint-staged',
+    'pre-push': 'yarn test',
     // 'commit-msg': 'commitlint -E HUSKY_GIT_PARAMS',
   },
 };

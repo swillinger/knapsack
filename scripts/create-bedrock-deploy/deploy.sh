9387c0f3ca8653088bd89834fcd781288206cdf2
Evan Lovely <evanlovely@gmail.com>
2018-11-16 15:19:31 -0800

    ci(create-bedrock): create-bedrock-deploy tweak

--- scripts/create-bedrock-deploy/deploy.sh
+++ scripts/create-bedrock-deploy/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -x
 npm i -g now --unsafe-perm
-now --build-env NPM_TOKEN=@npm-token
+now --token=$NOW_TOKEN --team=basalt --name='create-bedrock-deploy' --no-clipboard --build-env NPM_TOKEN=@npm-token

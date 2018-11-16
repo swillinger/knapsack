de18d49b2df15f5ca42dce2eac3138a11ca5fd61
Evan Lovely <evanlovely@gmail.com>
2018-11-16 15:36:00 -0800

    ci(*): cd dir

--- scripts/create-bedrock-deploy/deploy.sh
+++ scripts/create-bedrock-deploy/deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -x
+# Start in this directory even if ran elsewhere
+cd "$(dirname "$0")"
 npm i -g now --unsafe-perm
 now --token=$NOW_TOKEN --team=basalt --name='create-bedrock-deploy' --no-clipboard --build-env NPM_TOKEN=@npm-token

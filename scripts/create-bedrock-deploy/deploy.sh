6a7535e724684673984f786d2a12a8fdf1149892
Evan Lovely <EvanLovely@users.noreply.github.com>
2018-11-19 16:57:37 -0800

    Update deploy.sh

--- scripts/create-bedrock-deploy/deploy.sh
+++ scripts/create-bedrock-deploy/deploy.sh
@@ -2,7 +2,7 @@
 set -x
 # Start in this directory even if ran elsewhere
 cd "$(dirname "$0")"
-# npm i -g now --unsafe-perm
+npm i -g now --unsafe-perm
 now deploy --token=$NOW_TOKEN --team=basalt --no-clipboard --build-env NPM_TOKEN=@npm-token
 now alias --token=$NOW_TOKEN --team=basalt
 now rm create-bedrock-demo --safe --yes

5fd78f367952a4042f89fb22c01bc616716bb5d5
Evan Lovely <evanlovely@gmail.com>
2018-11-27 22:41:13 -0800

    feat(create-bedrock): tighten up design tokens
    affects: @basalt/bedrock, @basalt/bedrock-example-design-token-mania,
    @basalt/bedrock-example-simple, create-bedrock
    
    also making a new examples/design-token-mania for all the tokens!

--- bedrock/src/lib/constants.js
+++ bedrock/src/lib/constants.js
@@ -4,43 +4,43 @@ const {
 } = require('@basalt/bedrock-design-token-demos/constants');
 
 const TOKEN_GROUPS = {
-  SHADOWS: {
-    id: 'shadows',
-    title: 'Shadows',
-    description: 'Some shadows',
-    tokenCategoryIds: [
-      TOKEN_CATS.BOX_SHADOW,
-      TOKEN_CATS.INNER_SHADOW,
-      TOKEN_CATS.TEXT_SHADOW,
-    ],
-  },
-  BORDERS: {
-    id: 'borders',
-    title: 'Borders',
-    description: 'Some borders',
-    tokenCategoryIds: [
-      TOKEN_CATS.BORDER_COLOR,
-      TOKEN_CATS.BORDER_STYLE,
-      TOKEN_CATS.BORDER_RADIUS,
-    ],
-  },
   COLORS: {
     id: 'colors',
     title: 'Colors',
     description: 'Some colors',
     tokenCategoryIds: [
       TOKEN_CATS.BACKGROUND_COLOR,
-      TOKEN_CATS.BORDER_COLOR,
+      // TOKEN_CATS.BORDER_COLOR,
       TOKEN_CATS.TEXT_COLOR,
       TOKEN_CATS.HR_COLOR,
     ],
   },
+  // BORDERS: {
+  //   id: 'borders',
+  //   title: 'Borders',
+  //   description: 'Some borders',
+  //   tokenCategoryIds: [
+  //     TOKEN_CATS.BORDER_COLOR,
+  //     TOKEN_CATS.BORDER_STYLE,
+  //     TOKEN_CATS.BORDER_RADIUS,
+  //   ],
+  // },
   // @todo review
-  ANIMATIONS: {
-    id: 'animations',
-    title: 'Animations',
-    description: 'Some Animations',
-    tokenCategoryIds: [TOKEN_CATS.ANIMATION],
+  // ANIMATIONS: {
+  //   id: 'animations',
+  //   title: 'Animations',
+  //   description: 'Some Animations',
+  //   tokenCategoryIds: [TOKEN_CATS.ANIMATION],
+  // },
+  SIZING: {
+    id: 'sizing',
+    title: 'Sizing',
+    description: 'Some sizing',
+    tokenCategoryIds: [
+      TOKEN_CATS.SPACING,
+      TOKEN_CATS.MEDIA_QUERY,
+      // TOKEN_CATS.FONT_SIZE,
+    ],
   },
   TYPOGRAPHY: {
     id: 'typography',
@@ -49,21 +49,22 @@ const TOKEN_GROUPS = {
     tokenCategoryIds: [
       TOKEN_CATS.FONT_FAMILY,
       TOKEN_CATS.FONT_SIZE,
-      TOKEN_CATS.FONT_STYLE,
-      TOKEN_CATS.FONT_WEIGHT,
+      // these two are kinda boring; just bold or italic. should we keep?
+      // TOKEN_CATS.FONT_STYLE,
+      // TOKEN_CATS.FONT_WEIGHT,
       TOKEN_CATS.LINE_HEIGHT,
       TOKEN_CATS.TEXT_COLOR,
       TOKEN_CATS.TEXT_SHADOW,
     ],
   },
-  SIZING: {
-    id: 'sizing',
-    title: 'Sizing',
-    description: 'Some sizing',
+  SHADOWS: {
+    id: 'shadows',
+    title: 'Shadows',
+    description: 'Some shadows',
     tokenCategoryIds: [
-      TOKEN_CATS.SPACING,
-      TOKEN_CATS.MEDIA_QUERY,
-      TOKEN_CATS.FONT_SIZE,
+      TOKEN_CATS.BOX_SHADOW,
+      TOKEN_CATS.INNER_SHADOW,
+      TOKEN_CATS.TEXT_SHADOW,
     ],
   },
 };
---
+++ examples/design-token-mania/bedrock.config.js
@@ -0,0 +1,21 @@
+const twigRenderer = require('./twig-renderer');
+
+/** @type {BedrockConfig} */
+const config = {
+  patterns: ['./assets/patterns/*'],
+  newPatternDir: './assets/patterns/',
+  dist: './dist',
+  public: './public',
+  data: './data',
+  assets: './assets',
+  css: ['./public/assets/simple.css'],
+  // js: ['./public/assets/script.js'],
+  templates: [{
+    test: theTemplatePath => theTemplatePath.endsWith('.twig'),
+    render: (template, data = {}) => twigRenderer.render(template, data),
+    renderString: (templateString, data = {}) => twigRenderer.renderString(templateString, data),
+  }],
+  designTokens: './design-tokens/tokens.yml',
+};
+
+module.exports = config;

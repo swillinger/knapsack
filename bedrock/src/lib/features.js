b1f1d013df7c789b5ebf88554aa7c88f5a89f24d
Evan Lovely <evanlovely@gmail.com>
2018-11-27 22:03:45 -0800

    fix(bedrock): enabling UI settings for staging, for now
    affects: @basalt/bedrock
    
    just want to be able to show off Bedrock on the staging site for awhile... will need to disable
    again soon

--- bedrock/src/lib/features.js
+++ bedrock/src/lib/features.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 // do not import this from within the `client/` folder - it's available via `context.features` using React Context
 module.exports = {
   enableBlockquotes: false,
-  enableUiSettings: !isProd,
+  enableUiSettings: true, // @todo disable UI settings again soon; normally it's `!isProd` turning, but I'm turning it on for staging sites so we can show off Bedrock editing for awhile
   // @todo fix ability to create new patterns via UI
   enableUiCreatePattern: false,
   enableTemplatePush: !isProd,

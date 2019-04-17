d1c3c37153846e49a0d2bc2da0a550af9c5c27e4
Evan Lovely <evanlovely@gmail.com>
2019-04-17 10:12:52 -0700

    ci: create github release using new lerna version option instead of our custom script

--- scripts/release.sh
+++ scripts/release.sh
@@ -19,11 +19,15 @@ git config --global user.name "Knapsack Bot"
 cp ./scripts/.npmrc-ci ~/.npmrc
 PREV_VERSION=`git describe --abbrev=0`
 # see `lerna.json` for options
-./node_modules/.bin/lerna publish --conventional-commits --yes
+GH_TOKEN=$GITHUB_TOKEN ./node_modules/.bin/lerna version --github-release --conventional-commits --yes
+echo 'done with lerna version'
+echo ''
+./node_modules/.bin/lerna publish --yes
 CURRENT_VERSION=`git describe --abbrev=0`
 echo "Previous version: $PREV_VERSION Current Version: $CURRENT_VERSION"
 echo "changelog test output:"
 CHANGES="`git show $PREV_VERSION:CHANGELOG.md | diff -u - CHANGELOG.md | grep '^\+' | grep -v '^\++' | sed -E 's/^\+//'`"
-echo "Creating GitHub release and announcing to issues"
-echo "$CHANGES" | node ./scripts/create-github-release.js "$CURRENT_VERSION"
+#echo "Creating GitHub release and announcing to issues"
+echo "$CHANGES"
+#echo "$CHANGES" | node ./scripts/create-github-release.js "$CURRENT_VERSION"
 echo "END: changelog"

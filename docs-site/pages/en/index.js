c763860c01bc71731df1f2db01dce221bc58a57a
Jake Love <jake@basalt.io>
2019-06-20 16:56:39 -0700

    feat: complete porting of landing page

--- docs-site/pages/en/index.js
+++ docs-site/pages/en/index.js
@@ -29,15 +29,9 @@ class HomeSplash extends React.Component {
       </div>
     );
 
-    const Logo = props => (
-      <div className="projectLogo">
-        <img src={props.img_src} alt="Project Logo" />
-      </div>
-    );
-
     const ProjectTitle = () => (
       <h2 className="projectTitle">
-        {siteConfig.title}
+        Welcome to {siteConfig.title}
         <small>{siteConfig.tagline}</small>
       </h2>
     );
@@ -60,13 +54,11 @@ class HomeSplash extends React.Component {
 
     return (
       <SplashContainer>
-        <Logo img_src={`${baseUrl}img/docusaurus.svg`} />
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <a href="#getting-started" className="button button--c2a">Install</a>
+            <a href={docUrl('getting-started')} className="button">Getting Started</a>
           </PromoSection>
         </div>
       </SplashContainer>
@@ -77,14 +69,28 @@ class HomeSplash extends React.Component {
 class Index extends React.Component {
   render() {
     const { config: siteConfig, language = '' } = this.props;
-    const { baseUrl } = siteConfig;
+    const { baseUrl, docsUrl } = siteConfig;
+    const docsPart = `${docsUrl ? `${docsUrl}/` : ''}`;
+    const langPart = `${language ? `${language}/` : ''}`;
+    const docUrl = doc => `${baseUrl}${docsPart}${langPart}${doc}`;
+
+    const PromoSection = props => (
+      <div className="section promoSection">
+        <div className="promoRow">
+          <div className="pluginRowBlock">{props.children}</div>
+        </div>
+      </div>
+    );
 
     const Block = props => (
       <Container
-        padding={['bottom', 'top']}
         id={props.id}
         background={props.background}
+        className={props.className}
       >
+        {props.title &&
+          <h1 className="blockSectionTitle">{props.title}</h1>
+        }
         <GridBlock
           align="center"
           contents={props.children}
@@ -93,70 +99,55 @@ class Index extends React.Component {
       </Container>
     );
 
-    const FeatureCallout = () => (
-      <div
-        className="productShowcaseSection paddingBottom"
-        style={{ textAlign: 'center' }}
-      >
-        <h2>Feature Callout</h2>
-        <MarkdownBlock>These are features of this project</MarkdownBlock>
+    const GettingStarted = () => (
+      <div className="gettingStartedBlock" id="getting-started">
+        <h1 className="blockSectionTitle">Getting Started</h1>
+        <code>
+          npm create knapsack my-design-system<br />
+          cd my-design-system<br />
+          npm install<br />
+          npm start
+        </code>
+        <PromoSection>
+          <a href={docUrl('getting-started')} className="button">Read the Docs</a>
+          <a href={siteConfig.repoUrl} className="button button--c2a" target="_blank">GitHub</a>
+        </PromoSection>
       </div>
     );
 
-    const TryOut = () => (
-      <Block id="try">
-        {[
-          {
-            content: 'Talk about trying this out',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'left',
-            title: 'Try it Out',
-          },
-        ]}
-      </Block>
-    );
-
-    const Description = () => (
-      <Block background="dark">
-        {[
-          {
-            content:
-              'This is another description of how this project is useful',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'right',
-            title: 'Description',
-          },
-        ]}
-      </Block>
-    );
-
-    const LearnHow = () => (
-      <Block background="light">
-        {[
-          {
-            content: 'Talk about learning how to use this',
-            image: `${baseUrl}img/docusaurus.svg`,
-            imageAlign: 'right',
-            title: 'Learn How',
-          },
-        ]}
-      </Block>
+    const LearnMore = () => (
+      <div className="learnMoreBlock" style={{ textAlign: 'center' }}>
+        <h1 className="blockSectionTitle">Want to learn more about Knapsack?</h1>
+        <a href="mailto:knapsack@basalt.io" className="button">Contact Us</a>
+      </div>
     );
 
     const Features = () => (
-      <Block layout="fourColumn">
+      <Block className="featuresBlock" layout="fourColumn" title="A new, open source, scalable platform for multi-brand design systems">
         {[
           {
-            content: 'This is the content of my feature',
-            image: `${baseUrl}img/docusaurus.svg`,
+            content: 'Allows integration points cross-discipline',
+            image: `/icon/route.svg`,
             imageAlign: 'top',
-            title: 'Feature One',
+            title: '',
           },
           {
-            content: 'The content of my second feature',
-            image: `${baseUrl}img/docusaurus.svg`,
+            content: 'Facilitates documentation to ensure clear component intention, properties, and use cases',
+            image: `/icon/documentation.svg`,
             imageAlign: 'top',
-            title: 'Feature Two',
+            title: '',
+          },
+          {
+            content: 'Prototypes full experiences in our Page Builder',
+            image: `/icon/pagebuilder.svg`,
+            imageAlign: 'top',
+            title: '',
+          },
+          {
+            content: 'Builds coded components for your design system',
+            image: `/icon/component.svg`,
+            imageAlign: 'top',
+            title: '',
           },
         ]}
       </Block>
@@ -170,22 +161,29 @@ class Index extends React.Component {
       const showcase = siteConfig.users
         .filter(user => user.pinned)
         .map(user => (
-          <a href={user.infoLink} key={user.infoLink}>
-            <img src={user.image} alt={user.caption} title={user.caption} />
-          </a>
+          <figure className="user-badge" key={user.id}>
+            <div className="img-wrapper">
+              <img src={user.image} alt="" />
+            </div>
+            <figcaption>
+              <blockquote>{user.caption}</blockquote>
+              <h5>- {user.byline}</h5>
+            </figcaption>
+          </figure>
         ));
 
       const pageUrl = page => baseUrl + (language ? `${language}/` : '') + page;
 
       return (
-        <div className="productShowcaseSection paddingBottom">
-          <h2>Who is Using This?</h2>
-          <p>This project is used by all these people</p>
-          <div className="logos">{showcase}</div>
-          <div className="more-users">
-            <a className="button" href={pageUrl('users.html')}>
-              More {siteConfig.title} Users
-            </a>
+        <div className="productShowcaseSection">
+          <div className="inner">
+            <h1 className="blockSectionTitle">What are people saying about Knapsack?</h1>
+            <div className="user-wrapper">{showcase}</div>
+            {/*<div className="more-users">*/}
+              {/*<a className="button" href={pageUrl('users.html')}>*/}
+                {/*More {siteConfig.title} Users*/}
+              {/*</a>*/}
+            {/*</div>*/}
           </div>
         </div>
       );
@@ -196,11 +194,9 @@ class Index extends React.Component {
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
           <Features />
-          <FeatureCallout />
-          <LearnHow />
-          <TryOut />
-          <Description />
+          <GettingStarted />
           <Showcase />
+          <LearnMore />
         </div>
       </div>
     );

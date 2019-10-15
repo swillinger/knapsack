e1554b86f9dc6336d60a6e8046e675339e752e5b
Evan Lovely <evanlovely@gmail.com>
2019-10-15 11:12:12 -0700

    ci: ensure cli is always symlinked

---
+++ knapsack/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const currentNodeVersion = process.versions.node;
+const majorNodeVersion = parseInt(currentNodeVersion.split('.')[0], 10);
+
+// The minimum we want is node v8.9.0 which is LTS Carbon, released Oct 2017
+if (majorNodeVersion > 10) {
+  console.error(
+    `You are running Node ${currentNodeVersion} and this requires Node 10 or higher. Please update your version of Node: https://nodejs.org/en/`,
+  );
+  process.exit(1);
+}
+
+const cliFile = path.join(__dirname, './dist/cli/knapsack-cli.js');
+
+if (!fs.existsSync(cliFile)) {
+  console.error(
+    'The main cli entry file for Knapsack does not exists, which probably means that it was not built and you are developing Knapsack, so please run "yarn build" in this folder:',
+  );
+  console.error(__dirname);
+  process.exit(1);
+}
+
+require(cliFile);

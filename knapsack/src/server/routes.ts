ec3bb4789bcecdbc16f5a140dd43fd7c8431d762
Evan Lovely <evanlovely@gmail.com>
2019-11-09 22:58:43 -0800

    fix: restore server routes file

---
+++ knapsack/src/server/routes.ts
@@ -0,0 +1,108 @@
+import express from 'express';
+import { join } from 'path';
+import { apiUrlBase } from '../lib/constants';
+
+const router = express.Router();
+
+export function setupRoutes({
+  patterns,
+  knapsackDistDir,
+  distDir,
+  publicDir,
+}: {
+  patterns: import('./patterns').Patterns;
+  knapsackDistDir: string;
+  distDir?: string;
+  publicDir?: string;
+}): typeof router {
+  router.use('*', async (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept',
+    );
+    next();
+  });
+
+  router.use(`${apiUrlBase}`, (req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+      next();
+    } else {
+      // faking a slight delay in local development to better reflect re-world delays
+      setTimeout(next, 500);
+    }
+  });
+
+  router.use(
+    express.static(knapsackDistDir, {
+      maxAge: '1d',
+    }),
+  );
+
+  if (distDir) {
+    router.use(
+      express.static(distDir, {
+        maxAge: '1d',
+      }),
+    );
+  }
+
+  if (publicDir) {
+    router.use(express.static(publicDir));
+  }
+
+  // Since this is a Single Page App, we will send all html requests to the `index.html` file in the dist
+  router.use('*', (req, res, next) => {
+    const { accept = '' } = req.headers;
+    const accepted = accept.split(',');
+    // this is for serving up a Netlify CMS folder if present
+    if (!req.baseUrl.startsWith('/admin') && accepted.includes('text/html')) {
+      res.sendFile(join(knapsackDistDir, 'index.html'));
+    } else {
+      next();
+    }
+  });
+
+  // This page is mainly so IE can get a list of links to view the individual templates outside of the system
+  router.route('/demo-urls').get((req, res) => {
+    const patternDemos = patterns.getPatternsDemoUrls();
+
+    /* eslint-disable prettier/prettier */
+    // disabling prettier so it's possible to keep indenting semi-similar to how it'd be done with templates, please try and keep it tidy and consistent!
+    res.send(`
+<ul>
+${patternDemos
+  .map(
+    patternDemo => `
+  <li>
+    Pattern: ${patternDemo.title}
+    <ul>
+      ${patternDemo.templates
+        .map(
+          template => `
+        <li>
+          Template: ${template.title}
+          <br>
+            ${template.demoUrls
+              .map(
+                (demoUrl, i) => `
+              <a href="${demoUrl}" target="_blank">Demo Data ${i + 1}</a>
+            `,
+              )
+              .join(' - ')}
+        </li>
+      `,
+        )
+        .join('\n')}
+    </ul>
+  </li>
+`,
+  )
+  .join('\n')}
+</ul>
+    `);
+    /* eslint-enable prettier/prettier */
+  });
+
+  return router;
+}

c5771a6e266c902b986ba0af66a994de6ac1e85b
Brandon Reid <brandonreidak@gmail.com>
2019-11-27 13:47:56 -0800

    fix: remove classname from text-input-wrapper

--- design-system/src/atoms/forms/text-input-wrapper.tsx
+++ design-system/src/atoms/forms/text-input-wrapper.tsx
@@ -1,22 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import cn from 'classnames';
 import './text-input-wrapper.scss';
 
 export function TextInputWrapper(props) {
-  return (
-    <div className={cn('ks-text-input-wrapper', props.className)}>
-      {props.children}
-    </div>
-  );
+  return <div className="ks-text-input-wrapper">{props.children}</div>;
 }
 
 TextInputWrapper.defaultProps = {
   children: null,
-  className: null,
 };
 
 TextInputWrapper.propTypes = {
   children: PropTypes.element,
-  className: PropTypes.string,
 };

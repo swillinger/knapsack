c789bc4c164c50461e1ccc492b06d47dcb04fcc9
Evan Lovely <evanlovely@gmail.com>
2019-11-28 09:15:21 -0800

    fix: remove "editMode"

--- knapsack/src/client/layouts/page-with-sidebar.tsx
+++ knapsack/src/client/layouts/page-with-sidebar.tsx
@@ -22,13 +22,7 @@ import ErrorCatcher from '../utils/error-catcher';
 import { SiteHeaderConnected } from '../components/site-header';
 import { PageHeaderContainer } from '../components/page-header';
 import './page-with-sidebar.scss';
-import {
-  useDispatch,
-  useSelector,
-  saveToServer,
-  disableEditMode,
-  enableEditMode,
-} from '../store';
+import { useDispatch, useSelector, saveToServer } from '../store';
 
 type Props = {
   isInitiallyCollapsed?: boolean;
@@ -49,7 +43,6 @@ const PageWithSidebar: React.FC<Props> = ({
     isInitiallyCollapsed,
   );
   const canEdit = useSelector(s => s.userState.canEdit);
-  const isEditMode = useSelector(s => s.ui.isEditMode);
   const dispatch = useDispatch();
 
   return (
@@ -95,15 +88,6 @@ const PageWithSidebar: React.FC<Props> = ({
               >
                 Save it all
               </KsButton>
-              <KsButton
-                size="m"
-                onClick={() =>
-                  dispatch(isEditMode ? disableEditMode() : enableEditMode())
-                }
-              >
-                Turn edit mode {isEditMode ? 'off' : 'on'}
-              </KsButton>
-              <hr />
             </div>
           )}
           {children}
--- knapsack/src/client/store/ui.ts
+++ knapsack/src/client/store/ui.ts
@@ -4,8 +4,6 @@ type StatusTypes = 'success' | 'info' | 'warning' | 'error';
 
 const SET_STATUS = 'knapsack/ui/SET_STATUS';
 const REMOVE_STATUS = 'knapsack/ui/REMOVE_STATUS';
-const ENABLE_EDIT_MODE = 'knapsack/ui/ENABLE_EDIT_MODE';
-const DISABLE_EDIT_MODE = 'knapsack/ui/DISABLE_EDIT_MODE';
 
 type Status = {
   type: StatusTypes;
@@ -19,7 +17,6 @@ export interface UiState {
     message: string;
     dismissAfter?: number;
   };
-  isEditMode?: boolean;
 }
 
 interface SetStatusAction extends Action {
@@ -31,31 +28,7 @@ interface RemoveStatusAction extends Action {
   type: typeof REMOVE_STATUS;
 }
 
-interface EnableEditModeAction extends Action {
-  type: typeof ENABLE_EDIT_MODE;
-}
-
-export function enableEditMode(): EnableEditModeAction {
-  return {
-    type: ENABLE_EDIT_MODE,
-  };
-}
-
-interface DisableEditModeAction extends Action {
-  type: typeof DISABLE_EDIT_MODE;
-}
-
-export function disableEditMode(): DisableEditModeAction {
-  return {
-    type: DISABLE_EDIT_MODE,
-  };
-}
-
-type UiActionTypes =
-  | SetStatusAction
-  | RemoveStatusAction
-  | EnableEditModeAction
-  | DisableEditModeAction;
+type UiActionTypes = SetStatusAction | RemoveStatusAction;
 
 export function removeStatus(): RemoveStatusAction {
   return {
@@ -93,16 +66,6 @@ export default function reducer(
       return {
         ...state,
         status: null,
-      };
-    case ENABLE_EDIT_MODE:
-      return {
-        ...state,
-        isEditMode: true,
-      };
-    case DISABLE_EDIT_MODE:
-      return {
-        ...state,
-        isEditMode: false,
       };
     default:
       return {

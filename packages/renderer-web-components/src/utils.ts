38750c025b1e2ffd913c7c2d8c8549884d42668b
Evan Lovely <EvanLovely@users.noreply.github.com>
2019-12-07 21:35:45 -0800

    feat: create web-component renderer

---
+++ packages/renderer-web-components/src/utils.ts
@@ -0,0 +1,49 @@
+import { compile } from 'ejs';
+import { join } from 'path';
+import { readFileSync } from 'fs';
+
+const renderUsageTemplate = compile(
+  readFileSync(join(__dirname, 'template.web-components.ejs'), 'utf8'),
+  {
+    filename: 'template.web-components.ejs',
+    async: true,
+  },
+);
+
+export async function getUsage(data: {
+  templateName: string;
+  props?: object;
+  children?: string;
+  before?: string;
+  after?: string;
+}): Promise<string> {
+  const props = Object.keys(data.props || {}).map(key => {
+    const value = data.props[key];
+    return {
+      key,
+      value,
+    };
+  });
+  const attributes = props
+    .map(({ key, value }) => {
+      switch (typeof value) {
+        case 'string':
+          return `${key}="${value}"`;
+        case 'boolean':
+          return value ? `${key}` : `${key}="${value}"`;
+        default:
+          return `${key}="${JSON.stringify(value)}"`;
+      }
+    })
+    .join(' ');
+  const { templateName, children, before, after } = data;
+  const result = await renderUsageTemplate({
+    templateName,
+    attributes,
+    props,
+    children,
+    before,
+    after,
+  });
+  return result.trim();
+}

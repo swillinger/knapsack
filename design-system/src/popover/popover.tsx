66c44f17049367a182c2a57a24f2f8a283ab69a8
Evan Lovely <EvanLovely@users.noreply.github.com>
2019-12-23 10:04:37 -0800

    feat(design-system): popover improvements

--- design-system/src/popover/popover.tsx
+++ design-system/src/popover/popover.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Popover, { PopoverProps, ArrowContainer } from 'react-tiny-popover'; // https://www.npmjs.com/package/react-tiny-popover
 import './popover.scss';
 import { useHover } from '../utils/hooks';
@@ -6,6 +6,7 @@ import { useHover } from '../utils/hooks';
 type Props = Omit<PopoverProps, 'isOpen'> & {
   isOpen?: boolean;
   isHoverTriggered?: boolean;
+  trigger?: 'prop' | 'hover' | 'click';
 };
 
 export const KsPopover: React.FC<Props> = ({
@@ -13,17 +14,41 @@ export const KsPopover: React.FC<Props> = ({
   content,
   isOpen,
   children,
+  trigger = 'hover',
   ...rest
 }: Props) => {
   const [hoverRef, isHovered] = useHover();
+  const [isPopoverOpen, setOpen] = useState();
+  const handleChildTrigger = () => {
+    if (trigger === 'click') {
+      setOpen(cur => !cur);
+    }
+  };
   // @todo re-enable, fix offset arrow caused by margin
   const hasArrow = false;
 
+  let open;
+  switch (trigger) {
+    case 'click':
+      open = isPopoverOpen;
+      break;
+    case 'hover':
+      open = isHovered;
+      break;
+    case 'prop':
+      open = isOpen;
+      break;
+  }
   return (
     <Popover
       // @todo re-enable, then fix bug where you can see it but the z-index is too low (shows popover as a popunder)
       transitionDuration={0}
-      isOpen={isHoverTriggered ? (isHovered as boolean) : isOpen}
+      isOpen={open}
+      onClickOutside={() => {
+        if (trigger === 'click') {
+          setOpen(false);
+        }
+      }}
       content={({ position, targetRect, popoverRect }) => {
         const popoverContent = <div className="ks-popover__box">{content}</div>;
         if (hasArrow) {
@@ -44,7 +69,14 @@ export const KsPopover: React.FC<Props> = ({
       }}
       {...rest}
     >
-      <div className="ks-popover__children" ref={hoverRef as any}>
+      <div
+        className="ks-popover__children"
+        ref={hoverRef as any}
+        onClick={handleChildTrigger}
+        onKeyPress={handleChildTrigger}
+        role="button"
+        tabIndex={0}
+      >
         {children}
       </div>
     </Popover>
